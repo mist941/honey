ea0d08ae4aab22a3ad9019692accdf2611e9be2c
Ivan Statkevich <statkevich.ivan999@gmail.com>
2021-07-25 20:33:30 +0300

    working on shop page

--- src/components/pages/Shop/index.tsx
+++ src/components/pages/Shop/index.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import {PageTemplate} from '../../templates/PageTemplate';
+import styles from './style.module.scss';
+import {productList} from "./helper";
 
 export const ShopPage = () => {
   return (
     <PageTemplate title='Магазин'>
-      Shop page
+      <div className={styles['product-list']}>
+        {
+          productList.map(product => (
+            <div className={styles['product']} key={product.id}>
+              <div className={styles['img-wrapper']}>
+                <img src={product.image} alt=""/>
+              </div>
+              <p className={styles['name']}>{product.name}</p>
+              <p className={styles['cost']}>
+                <span>{product.cost}</span> грн
+              </p>
+              <p className={styles['amount']}>
+                В наличи - <span>{product.amount}</span>
+              </p>
+            </div>
+          ))
+        }
+      </div>
     </PageTemplate>
   );
 };
